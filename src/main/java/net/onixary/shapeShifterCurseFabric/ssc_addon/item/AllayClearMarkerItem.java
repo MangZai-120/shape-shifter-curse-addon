92aa42e88e2b960796f553f8ccaa24671fc33d5e
宋明禹(Song Mingyu) <songmingyu20060301@163.com>
2026-02-21 17:33:28 +0800

    添加和消除友军标记的物品做好，但没法测试

---
+++ src/main/java/net/onixary/shapeShifterCurseFabric/ssc_addon/item/AllayClearMarkerItem.java
@@ -0,0 +1,55 @@
+package net.onixary.shapeShifterCurseFabric.ssc_addon.item;
+
+import io.github.apace100.apoli.component.PowerHolderComponent;
+import io.github.apace100.apoli.power.PowerType;
+import io.github.apace100.apoli.power.PowerTypeRegistry;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.stat.Stats;
+import net.minecraft.util.Hand;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+import net.onixary.shapeShifterCurseFabric.ssc_addon.entity.AllayClearMarkerEntity;
+
+public class AllayClearMarkerItem extends Item {
+    private static final Identifier REQUIRED_POWER = new Identifier("my_addon", "form_allay_sp_group_heal");
+
+    public AllayClearMarkerItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
+        if (!world.isClient && entity instanceof PlayerEntity player) {
+            PowerType<?> powerType = PowerTypeRegistry.get(REQUIRED_POWER);
+            if (powerType == null || !PowerHolderComponent.KEY.get(player).hasPower(powerType)) {
+                stack.setCount(0);
+            }
+        }
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        ItemStack itemStack = user.getStackInHand(hand);
+        world.playSound(null, user.getX(), user.getY(), user.getZ(), SoundEvents.ENTITY_SNOWBALL_THROW, SoundCategory.NEUTRAL, 0.5F, 0.4F / (world.getRandom().nextFloat() * 0.4F + 0.8F));
+        
+        if (!world.isClient) {
+            AllayClearMarkerEntity entity = new AllayClearMarkerEntity(world, user);
+            entity.setItem(itemStack);
+            entity.setVelocity(user, user.getPitch(), user.getYaw(), 0.0F, 1.5F, 1.0F);
+            world.spawnEntity(entity);
+        }
+
+        user.incrementStat(Stats.USED.getOrCreateStat(this));
+        if (!user.getAbilities().creativeMode) {
+            itemStack.decrement(1);
+        }
+
+        return TypedActionResult.success(itemStack, world.isClient());
+    }
+}
