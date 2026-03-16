7344b342733086d1e3841955215b8b4a98828145
wuhenqiubai <wth110628@163.com>
2026-03-16 21:36:30 +0800

    对mixin目录下的类进行分类

---
+++ src/main/java/net/onixary/shapeShifterCurseFabric/ssc_addon/mixin/entity/SscAddonLivingEntityMixin.java
@@ -0,0 +1,77 @@
+package net.onixary.shapeShifterCurseFabric.ssc_addon.mixin.entity;
+
+import io.github.apace100.apoli.component.PowerHolderComponent;
+import net.minecraft.entity.EntityGroup;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.onixary.shapeShifterCurseFabric.ssc_addon.power.EffectEfficiencyReductionPower;
+import net.onixary.shapeShifterCurseFabric.ssc_addon.util.FormIdentifiers;
+import net.onixary.shapeShifterCurseFabric.ssc_addon.util.FormUtils;
+import net.onixary.shapeShifterCurseFabric.ssc_addon.util.UndeadNeutralState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LivingEntity.class)
+public abstract class SscAddonLivingEntityMixin {
+
+    /**
+     * 亡灵被SP阿努比斯玩家攻击时，记录全局挑衅状态。
+     * damage方法定义在LivingEntity上，必须在此注入。
+     */
+    @Inject(method = "damage", at = @At("HEAD"))
+    private void ssc_addon$onUndeadDamaged(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+        LivingEntity self = (LivingEntity)(Object)this;
+        if (self instanceof MobEntity mob
+                && mob.getGroup() == EntityGroup.UNDEAD
+                && source.getAttacker() instanceof PlayerEntity player
+                && FormUtils.isForm(player, FormIdentifiers.ANUBIS_WOLF_SP)) {
+            UndeadNeutralState.PROVOKE_TIMESTAMPS.put(player.getUuid(), mob.getWorld().getTime());
+        }
+    }
+
+    @ModifyVariable(method = "addStatusEffect(Lnet/minecraft/entity/effect/StatusEffectInstance;Lnet/minecraft/entity/Entity;)Z", at = @At("HEAD"), argsOnly = true)
+    private StatusEffectInstance modifyStatusEffect(StatusEffectInstance effect) {
+        if (!effect.getEffectType().isInstant() && PowerHolderComponent.hasPower((LivingEntity)(Object)this, EffectEfficiencyReductionPower.class)) {
+            int originalAmp = effect.getAmplifier();
+            int newDuration;
+
+            // Logic:
+            // Level 1 (amp 0): Duration * 0.4 (40%)
+            // Level 2 (amp 1): Duration * 0.6 (60%), Amp -> 0
+            // Level 3 (amp 2): Duration * 0.8 (80%), Amp -> 0
+            // Level 4+ (amp 3+): Duration * 1.0 (100%), Amp -> 0
+
+            if (originalAmp == 0) {
+                // Level 1
+                newDuration = (int)(effect.getDuration() * 0.4);
+            } else if (originalAmp == 1) {
+                // Level 2
+                newDuration = (int)(effect.getDuration() * 0.6);
+            } else if (originalAmp == 2) {
+                // Level 3
+                newDuration = (int)(effect.getDuration() * 0.8);
+            } else {
+                // Level 4+
+                newDuration = effect.getDuration();
+            }
+
+            return new StatusEffectInstance(
+                effect.getEffectType(),
+                newDuration,
+                0, // Always force to Level 1 (amplifier 0)
+                effect.isAmbient(),
+                effect.shouldShowParticles(),
+                effect.shouldShowIcon(),
+		            null,
+                effect.getFactorCalculationData()
+            );
+        }
+        return effect;
+    }
+}
---
+++ src/main/java/net/onixary/shapeShifterCurseFabric/ssc_addon/mixin/input/StunnedInputMixin.java
@@ -0,0 +1,38 @@
+package net.onixary.shapeShifterCurseFabric.ssc_addon.mixin.input;
+
+import net.minecraft.client.MinecraftClient;
+import net.onixary.shapeShifterCurseFabric.ssc_addon.SscAddon;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(MinecraftClient.class)
+public class StunnedInputMixin {
+
+    @Inject(method = "doAttack", at = @At("HEAD"), cancellable = true)
+    private void onDoAttack(CallbackInfoReturnable<Boolean> cir) {
+        MinecraftClient client = (MinecraftClient) (Object) this;
+        if (client.player != null && client.player.hasStatusEffect(SscAddon.STUN)) {
+            cir.setReturnValue(false);
+        }
+    }
+
+    @Inject(method = "doItemUse", at = @At("HEAD"), cancellable = true)
+    private void onDoItemUse(CallbackInfo ci) {
+        MinecraftClient client = (MinecraftClient) (Object) this;
+        if (client.player != null && client.player.hasStatusEffect(SscAddon.STUN)) {
+            ci.cancel();
+        }
+    }
+
+    @Inject(method = "handleBlockBreaking", at = @At("HEAD"), cancellable = true)
+    private void onHandleBlockBreaking(boolean breaking, CallbackInfo ci) {
+        MinecraftClient client = (MinecraftClient) (Object) this;
+        if (client.player != null && client.player.hasStatusEffect(SscAddon.STUN) && breaking) {
+            ci.cancel();
+        }
+
+    }
+}
