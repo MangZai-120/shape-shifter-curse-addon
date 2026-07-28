62489f5061159232906ef75c2da3fad0caa43930
Jack "Pilot" Cooper <songmingyu20060301@163.com>
2026-07-28 10:49:21 +0800

    md映射弄成mojmap了

--- src/main/java/net/onixary/shapeShifterCurseFabric/ssc_addon/mixin/player/PotionBagDeathDropMixin.java
+++ src/main/java/net/onixary/shapeShifterCurseFabric/ssc_addon/mixin/player/PotionBagDeathDropMixin.java
@@ -21,10 +21,12 @@ public abstract class PotionBagDeathDropMixin {
 
 	/**
 	 * 死亡时掉落药水袋内物品（仅当 keepInventory 关闭时）
-	 * 注意：1.20.1 PlayerEntity 没有 dropInventory()，死亡掉落走 dropEquipment()（method_16078, ()V）
-	 * PlayerEntity 覆写了该方法（先 super.dropEquipment() 再掉玩家物品栏）
+	 * 1.20.1 yarn：PlayerEntity 覆写了 dropInventory()（method_16078, ()V），
+	 * 其内部在 keepInventory 关闭时执行 vanishCursedItems() + inventory.dropAll()。
+	 * 在 HEAD 注入：此时物品栏尚未 dropAll，可遍历找到药水袋并掉落其内物品。
+	 * 注意：dropEquipment 是 mojmap 名，yarn 名为 dropInventory，二者不可混用。
 	 */
-	@Inject(method = "dropEquipment()V", at = @At("HEAD"))
+	@Inject(method = "dropInventory()V", at = @At("HEAD"))
 	private void dropPotionBagItems(CallbackInfo ci) {
 		PlayerEntity player = (PlayerEntity) (Object) this;
 
